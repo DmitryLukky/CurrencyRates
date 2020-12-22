bba636515719846c34b833c3cc136243d004d824
DmitryLukky <dmitry.lukky@gmail.com>
2020-12-22 18:31:07 +0300

    Initial commit

---
+++ app/src/main/java/com/wexberry/currencyrates/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.wexberry.currencyrates
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
